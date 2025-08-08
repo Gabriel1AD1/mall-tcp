f24752b9fc4f868df4a4f99a200629cf1d0eaef8
gabo15@gmail.com <cerroteberes@gmail.com>
2025-08-08 11:34:34 -0500

    Agregando logs para ver el porque no inicia

--- src/main/java/com/labotec/pe/infra/init/InitServerCommand.java
+++ src/main/java/com/labotec/pe/infra/init/InitServerCommand.java
@@ -3,7 +3,11 @@ package com.labotec.pe.infra.init;
 import com.labotec.pe.app.port.input.DeviceService;
 import com.labotec.pe.domain.enums.DeviceStatus;
 import com.labotec.pe.infra.database.repo.DeviceEntityRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
@@ -13,10 +17,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Order()
 public class InitServerCommand implements ApplicationRunner {
+    private static final Logger log = LoggerFactory.getLogger(InitServerCommand.class);
     private final DeviceEntityRepository deviceEntityRepository;
-
+    @Value("${server.port.tcp}")
+    private Long tcpServerPort;
     @Override
     public void run(ApplicationArguments args) {
+        log.info("Inicializando servidor TCP en puerto {}", tcpServerPort);
         deviceEntityRepository.updateStatus(DeviceStatus.offline);
     }
 }
