c738a926ce9dbe13a08934b1175eaaa50261356f
Gabriel <cerroteberes@gmail.com>
2025-12-30 18:18:19 -0500

    Refactor sendCommand method to include message type in output

--- src/main/java/com/labotec/pe/infra/server/adapter/AdapterTcpDeviceService.java
+++ src/main/java/com/labotec/pe/infra/server/adapter/AdapterTcpDeviceService.java
@@ -1,6 +1,5 @@
 package com.labotec.pe.infra.server.adapter;
 
-import com.labotec.pe.app.constants.util.StatusLogin;
 import com.labotec.pe.app.port.output.tcp.TcpDeviceService;
 import com.labotec.pe.infra.server.ActiveChannelsRegistry;
 import com.labotec.pe.infra.server.MessageHandler;
@@ -11,19 +10,22 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class AdapterTcpDeviceService implements TcpDeviceService {
-    public static final String LINE_BREAK = "\r\n";
+  public static final String LINE_BREAK = "\r\n";
+  public static final String TYPE_MESSAGE = "#M#";
 
-    @Override
-    public boolean sendCommand(String imei, String command) {
-        ActiveChannelsRegistry.getChannel(imei).ifPresent(
-                channelHandlerContext -> {
-                    // Convierte el mensaje a ByteBuf
-                    var responseBuf = MessageHandler.handleOutput(command + LINE_BREAK);
-                    // Envía el mensaje al cliente con un listener para manejar errores
-                    channelHandlerContext.write(responseBuf).addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
-                    channelHandlerContext.flush();
-                }
-        );
-        return ActiveChannelsRegistry.getChannel(imei).isPresent();
-    }
+  @Override
+  public boolean sendCommand(String imei, String command) {
+    ActiveChannelsRegistry.getChannel(imei)
+        .ifPresent(
+            channelHandlerContext -> {
+              // Convierte el mensaje a ByteBuf
+              var responseBuf = MessageHandler.handleOutput(TYPE_MESSAGE + command + LINE_BREAK);
+              // Envía el mensaje al cliente con un listener para manejar errores
+              channelHandlerContext
+                  .write(responseBuf)
+                  .addListener(ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE);
+              channelHandlerContext.flush();
+            });
+    return ActiveChannelsRegistry.getChannel(imei).isPresent();
+  }
 }
